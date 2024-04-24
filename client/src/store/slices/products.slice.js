bcdd685de5eca8f940af960d65331f5c4aa4f866
rony171998 <rony171998@gmail.com>
2024-04-23 23:20:02 -0500

    upload image to s3

--- client/src/store/slices/products.slice.js
+++ client/src/store/slices/products.slice.js
@@ -1,106 +1,135 @@
-import { createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
-import { swal } from '../../components';
-import { setIsLoading } from './isLoading.slice';
-import { getMyProducts } from './user.slice';
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { swal } from "../../components";
+import { setIsLoading } from "./isLoading.slice";
+import { getMyProducts } from "./user.slice";
 
-if (process.env.NODE_ENV === 'development') {
-    axios.defaults.baseURL = 'http://localhost:4000/api/v1';
+if (process.env.NODE_ENV === "development") {
+    axios.defaults.baseURL = "http://localhost:4000/api/v1";
     //axios.defaults.baseURL = 'https://ecommerce-express.azurewebsites.net/api/v1';
-     
 } else {
-    axios.defaults.baseURL = 'https://ecommerce-express.azurewebsites.net/api/v1';  
+    axios.defaults.baseURL =
+        "https://ecommerce-express.azurewebsites.net/api/v1";
 }
 
 export const productsSlice = createSlice({
-    
-    name: 'products',
+    name: "products",
     initialState: [],
     reducers: {
         setProducts: (state, action) => {
-            return action.payload
-        }
+            return action.payload;
+        },
     },
-    
+});
+export const { setProducts } = productsSlice.actions;
 
-})
-export const { setProducts  } = productsSlice.actions;
-
-export const getProducts = () => (dispatch) => {
+export const getProducts = () => dispatch => {
     dispatch(setIsLoading(true));
-    
-    return axios.get(`/products`)
-        .then(res =>dispatch(setProducts(res.data.products)))
+
+    return axios
+        .get(`/products`)
+        .then(res => dispatch(setProducts(res.data.products)))
         .catch(err => console.log(err))
         .finally(() => dispatch(setIsLoading(false)));
-}
+};
 
-export const filterCategory = (id) => (dispatch) => {
+export const filterCategory = id => dispatch => {
     dispatch(setIsLoading(true));
-    return axios.get(`/products`)
-        .then((res) => {
+    return axios
+        .get(`/products`)
+        .then(res => {
             const productsSearched = res.data.data.products.filter(
-                (productsItem) => productsItem.category.id === Number(id)
+                productsItem => productsItem.category.id === Number(id)
             );
             dispatch(setProducts(productsSearched));
         })
         .finally(() => dispatch(setIsLoading(false)));
-        
-}
+};
 
-export const filterProductsByName = (name) => (dispatch) => {
+export const filterProductsByName = name => dispatch => {
     dispatch(setIsLoading(true));
-    return axios.get("/products")
-        .then((res) => {
-        const productsSearched = res.data.data.products.filter(
-            (productsItem) => productsItem.title.toLowerCase().includes(name)
-        );
-        dispatch(setProducts(productsSearched));
+    return axios
+        .get("/products")
+        .then(res => {
+            const productsSearched = res.data.data.products.filter(
+                productsItem => productsItem.title.toLowerCase().includes(name)
+            );
+            dispatch(setProducts(productsSearched));
         })
-        .finally(() => dispatch(setIsLoading(false)));      
-}
+        .finally(() => dispatch(setIsLoading(false)));
+};
 
-export const filterProductsByPrices = (priceMin , priceMax) => (dispatch) => {
+export const filterProductsByPrices = (priceMin, priceMax) => dispatch => {
     dispatch(setIsLoading(true));
-    return axios.get("/products")
-        .then((res) => {
-        const productsSearched = res.data.data.products.filter(
-            (productsItem) => productsItem.price >= priceMin  && productsItem.price <= priceMax
-        );
-        dispatch(setProducts(productsSearched));
+    return axios
+        .get("/products")
+        .then(res => {
+            const productsSearched = res.data.data.products.filter(
+                productsItem =>
+                    productsItem.price >= priceMin &&
+                    productsItem.price <= priceMax
+            );
+            dispatch(setProducts(productsSearched));
         })
-        .finally(() => dispatch(setIsLoading(false)));      
-}
+        .finally(() => dispatch(setIsLoading(false)));
+};
 
 const getConfig = () => ({
-    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
 });
 
-export const  postProduct = (formData) =>  (dispatch) => {   
+export const postProduct = formData => dispatch => {
     dispatch(setIsLoading(true));
-    
-    return axios.post ("/products", formData, getConfig())
-        .then((res) => dispatch(swal( "success" , res.statusText , "success") ))
-        .catch((err) => dispatch(console.log(err) , swal("Error", err.response.data.message, "error")))
+
+    return axios
+        .post("/products", formData, getConfig())
+        .then(res => dispatch(swal("success", res.statusText, "success")))
+        .catch(err =>
+            dispatch(
+                console.log(err),
+                swal("Error", err.response.data.message, "error")
+            )
+        )
         .finally(() => dispatch(setIsLoading(false)))
         .finally(() => dispatch(getProducts()));
-}
-export const pachProduct = (id , data) => (dispatch) => {
+};
+export const pachProduct = (id, data) => dispatch => {
     dispatch(setIsLoading(true));
-    return axios.patch(`/products/${id}`,data, getConfig())
-        .then((res) => dispatch(console.log(res) , swal( "success" , res.statusText , "success") ))
-        .catch((err) => dispatch(console.log(err) , swal("Error", err.response.data.message, "error")))
+    return axios
+        .patch(`/products/${id}`, data, getConfig())
+        .then(res =>
+            dispatch(
+                console.log(res),
+                swal("success", res.statusText, "success")
+            )
+        )
+        .catch(err =>
+            dispatch(
+                console.log(err),
+                swal("Error", err.response.data.message, "error")
+            )
+        )
         .finally(() => dispatch(setIsLoading(false)))
         .finally(() => dispatch(getProducts()));
-}
-export const deleteProduct = (id) => (dispatch) => {
+};
+export const deleteProduct = id => dispatch => {
     dispatch(setIsLoading(true));
-    return axios.delete(`/products/${id}`, getConfig())
-        .then((res) => dispatch(console.log(res) , swal( "success" , res.statusText , "success") ))
-        .catch((err) => dispatch(console.log(err) , swal("Error", err.response.data.message, "error")))
+    return axios
+        .delete(`/products/${id}`, getConfig())
+        .then(res =>
+            dispatch(
+                console.log(res),
+                swal("success", res.statusText, "success")
+            )
+        )
+        .catch(err =>
+            dispatch(
+                console.log(err),
+                swal("Error", err.response.data.message, "error")
+            )
+        )
         .finally(() => dispatch(setIsLoading(false)))
         .finally(() => dispatch(getMyProducts()));
-}
-
+};
 
 export default productsSlice.reducer;
--- server/controllers/products.controller.js
+++ server/controllers/products.controller.js
@@ -5,10 +5,13 @@ const { ProductImg } = require("../models/productImg.model");
 
 const { catchAsync } = require("../utils/catchAsync.util");
 const { storage } = require("../utils/firebase.util");
-const fs = require("fs");
+
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+
 const createProduct = catchAsync(async (req, res, next) => {
     const { title, description, price, categoryId, quantity } = req.body;
     const { sessionUser } = req;
+
     const newProduct = await Product.create({
         title,
         description,
@@ -19,40 +22,33 @@ const createProduct = catchAsync(async (req, res, next) => {
     });
 
     if (req.files.length > 0) {
-        const image = req.files.image;
-        const uploadPath = path.join(__dirname, "uploads", image.name);
+        const region = "us-east-2";
+        const client = new S3Client({
+            region,
+            credentials: {
+                accessKeyId: process.env.ACCESS_KEY_ID_S3,
+                secretAccessKey: process.env.SECRET_ACCESS_KEY_S3,
+            },
+        }); // Reemplaza "TU_REGION" con tu región de AWS S3
+        const Bucket = "mybucket-smart-mark"; // Reemplaza "NOMBRE_DE_TU_BUCKET" con el nombre de tu bucket en AWS S3
+        const filesPromises = req.files.map(async file => {
+            const Key = `imagesproduct/${Date.now()}_${file.originalname}`; // Ruta dentro del bucket donde se almacenará la imagen
 
-        image.mv(uploadPath, async err => {
-            if (err) {
-                return res.status(500).send(err);
-            }
-
-            // Sube la imagen al almacenamiento de Firebase
-            const remotePath = `imagesproduct/${image.name}`;
-            const dataImage = await bucket.upload(uploadPath, {
-                destination: remotePath,
+            const command = new PutObjectCommand({
+                Bucket,
+                Key,
+                Body: file.buffer,
             });
-            console.log("dataImage", dataImage);
 
-            // Elimina el archivo local después de subirlo a Firebase
-            fs.unlinkSync(uploadPath);
+            const response = await client.send(command);
 
-            res.send("Imagen subida y almacenada en Firebase exitosamente");
+            await ProductImg.create({
+                productId: newProduct.id,
+                imgUrl: `https://${Bucket}.s3.${region}.amazonaws.com/${Key}`, // Guarda la URL generada en la base de datos
+            });
         });
-        // const filesPromises = req.files.map(async file => {
-        //     const imgRef = ref(
-        //         storage,
-        //         `imagesproduct/${Date.now()}_${file.originalname}`
-        //     );
-        //     const imgRes = await uploadBytes(imgRef, file.buffer);
 
-        //     await ProductImg.create({
-        //         productId: newProduct.id,
-        //         imgUrl: imgRes.metadata.fullPath,
-        //     });
-        // });
-
-        // await Promise.all(filesPromises);
+        await Promise.all(filesPromises);
     }
 
     const productImgs = await ProductImg.findAll({
@@ -79,21 +75,21 @@ const getAllProduct = catchAsync(async (req, res, next) => {
         ],
     });
 
-    const newpro = products.map(async product => {
-        if (product.productImgs.length > 0) {
-            const productImgsPromises = product.productImgs.map(
-                async productImg => {
-                    const imgRef = ref(storage, productImg.imgUrl);
+    // const newpro = products.map(async product => {
+    //     if (product.productImgs.length > 0) {
+    //         const productImgsPromises = product.productImgs.map(
+    //             async productImg => {
+    //                 const imgRef = ref(storage, productImg.imgUrl);
 
-                    const imgFullPath = await getDownloadURL(imgRef);
+    //                 const imgFullPath = await getDownloadURL(imgRef);
 
-                    productImg.imgUrl = imgFullPath;
-                }
-            );
-            await Promise.all(productImgsPromises);
-        }
-    });
-    await Promise.all(newpro);
+    //                 productImg.imgUrl = imgFullPath;
+    //             }
+    //         );
+    //         await Promise.all(productImgsPromises);
+    //     }
+    // });
+    // await Promise.all(newpro);
 
     res.status(200).json({
         status: "success",
@@ -108,15 +104,15 @@ const getProductById = catchAsync(async (req, res, next) => {
         where: { productId: product.id },
     });
 
-    const productImgsPromises = productImgs.map(async productImg => {
-        const imgRef = ref(storage, productImg.imgUrl);
+    // const productImgsPromises = productImgs.map(async productImg => {
+    //     const imgRef = ref(storage, productImg.imgUrl);
 
-        const imgFullPath = await getDownloadURL(imgRef);
+    //     const imgFullPath = await getDownloadURL(imgRef);
 
-        productImg.imgUrl = imgFullPath;
-    });
+    //     productImg.imgUrl = imgFullPath;
+    // });
 
-    await Promise.all(productImgsPromises);
+    // await Promise.all(productImgsPromises);
 
     res.status(200).json({
         status: "success",
