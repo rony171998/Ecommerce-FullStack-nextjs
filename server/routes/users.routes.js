45fbb88b2b8ab94bb9e9ebdc62e71f5a2fdfec4e
rony171998 <rony171998@gmail.com>
2024-05-01 12:13:20 -0500

    Update users.routes.js

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,53 +1,51 @@
-const express = require('express');
-
-
-const {
-	getMeProducts,
-	createUser,
-	login,
-	getUserById,
-	updateUser,
-	deleteUser,
-	getUserOrdersAll,
-	getUserOrderById,
-	getAllorders
-} = require('../controllers/users.controller');
-
+const express = require("express");
 
 const {
-	createUserValidators
-} = require('../middlewares/validators.middleware');
+    getMeProducts,
+    createUser,
+    login,
+    getUserById,
+    updateUser,
+    deleteUser,
+    getUserOrdersAll,
+    getUserOrderById,
+    getAllorders,
+} = require("../controllers/users.controller");
 
-const { userExists } = require('../middlewares/users.middleware');
 const {
-	protectSession,
-	protectUserAccount,
-} = require('../middlewares/auth.middleware');
-const { orderExists } = require('../middlewares/orders.middleware');
+    createUserValidators,
+} = require("../middlewares/validators.middleware");
+
+const { userExists } = require("../middlewares/users.middleware");
+const {
+    protectSession,
+    protectUserAccount,
+} = require("../middlewares/auth.middleware");
+const { orderExists } = require("../middlewares/orders.middleware");
 
 const usersRouter = express.Router();
 
-usersRouter.post('/', createUserValidators, createUser);
+usersRouter.post("/", createUserValidators, createUser);
 
-usersRouter.post('/login', login);
+usersRouter.post("/login", login);
 
-usersRouter.get('/ordersAll', getAllorders);
+usersRouter.get("/ordersAll", getAllorders);
 
 usersRouter.use(protectSession);
 
-usersRouter.get('/me', getMeProducts);
+usersRouter.get("/me", getMeProducts);
 
-usersRouter.get('/orders', getUserOrdersAll);
+usersRouter.get("/orders", getUserOrdersAll);
 
-usersRouter.get('/:id',userExists, getUserById);
+usersRouter.get("/:id", userExists, getUserById);
 
-usersRouter.get('/orders/:id',orderExists, getUserOrderById);
+usersRouter.get("/orders/:id", orderExists, getUserOrderById);
 
 usersRouter
-	.use('/:id', userExists)
-	.route('/:id')
-	.get(getUserById)
-	.patch(protectUserAccount, updateUser)
-	.delete(protectUserAccount, deleteUser);
+    .use("/:id", userExists)
+    .route("/:id")
+    .get(getUserById)
+    .patch(protectUserAccount, updateUser)
+    .delete(protectUserAccount, deleteUser);
 
 module.exports = { usersRouter };
