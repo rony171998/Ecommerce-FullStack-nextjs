17e40a5d4a816427424c4afd6ea191b99ccb95d0
rony171998 <rony171998@gmail.com>
2024-06-16 10:43:34 -0500

    Update cart.controller.js

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -5,6 +5,19 @@ const { Order } = require("../models/order.model");
 
 const { catchAsync } = require("../utils/catchAsync.util");
 const { AppError } = require("../utils/appError.util");
+const { ProductImg } = require("../models/productImg.model");
+
+const { Hash } = require("@smithy/hash-node");
+const { parseUrl } = require("@smithy/url-parser");
+const { HttpRequest } = require("@smithy/protocol-http");
+const { S3RequestPresigner } = require("@aws-sdk/s3-request-presigner");
+const { formatUrl } = require("@aws-sdk/util-format-url");
+
+const region = process.env.REGION;
+const credentials = {
+    accessKeyId: process.env.ACCESS_KEY_ID_S3,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY_S3,
+};
 
 const createproductsinCart = catchAsync(async (req, res, next) => {
     const { sessionUser } = req;
@@ -39,7 +52,7 @@ const createproductsinCart = catchAsync(async (req, res, next) => {
         });
     } else {
         const productExists = await ProductsinCart.findOne({
-            where: { cartId: cart.id, productId , status: "active" },
+            where: { cartId: cart.id, productId, status: "active" },
         });
 
         if (productExists) {
@@ -55,8 +68,41 @@ const createproductsinCart = catchAsync(async (req, res, next) => {
 const getAllproductsinCart = catchAsync(async (req, res, next) => {
     const productsinCarts = await ProductsinCart.findAll({
         where: { status: "active" },
-        include: [{ model: Product }],
+        include: [
+            {
+                model: Product,
+                include: [
+                    {
+                        model: ProductImg,
+                        attributes: ["id", "imgUrl"],
+                        limit: 1, // Traer solo la primera imagen
+                    },
+                ],
+            },
+        ],
     });
+
+    const newProdsInCart = productsinCarts.map(async productInCart => {
+        const product = productInCart.Product;
+        if (product.ProductImgs.length > 0) {
+            const productImg = product.ProductImgs[0]; // Solo la primera imagen
+            if (productImg.imgUrl) {
+                const url = parseUrl(productImg.imgUrl);
+                const presigner = new S3RequestPresigner({
+                    credentials,
+                    region,
+                    sha256: Hash.bind(null, "sha256"),
+                });
+                const signedUrlObject = await presigner.presign(
+                    new HttpRequest(url)
+                );
+                productImg.imgUrl = formatUrl(signedUrlObject);
+            }
+        }
+        return productInCart;
+    });
+
+    await Promise.all(newProdsInCart);
 
     res.status(200).json({
         status: "success",
@@ -72,7 +118,7 @@ const getPurchases = catchAsync(async (req, res, next) => {
         include: [
             {
                 model: Cart,
-                
+
                 include: [
                     { model: ProductsinCart, include: [{ model: Product }] },
                 ],
@@ -87,7 +133,7 @@ const getPurchases = catchAsync(async (req, res, next) => {
 });
 
 const getPurchasesById = catchAsync(async (req, res, next) => {
-    const { order , sessionUser } = req;
+    const { order, sessionUser } = req;
 
     const purchases = await Order.findOne({
         where: { id: order.id, userId: sessionUser.id },
@@ -100,7 +146,6 @@ const getPurchasesById = catchAsync(async (req, res, next) => {
             },
         ],
     });
-
 
     res.status(200).json({
         status: "success",
@@ -198,13 +243,13 @@ const postCartPurchase = catchAsync(async (req, res, next) => {
 
     let totalPrice = 0;
 
-    const productsPurchasedPromises = cart.productsinCarts.map(async productInCart => {
-        
-            const newQty = productInCart.product.quantity - productInCart.quantity;
-                
+    const productsPurchasedPromises = cart.productsinCarts.map(
+        async productInCart => {
+            const newQty =
+                productInCart.product.quantity - productInCart.quantity;
 
-            const productPrice =productInCart.quantity * +productInCart.product.price;
-                
+            const productPrice =
+                productInCart.quantity * +productInCart.product.price;
 
             totalPrice += productPrice;
 
@@ -222,7 +267,6 @@ const postCartPurchase = catchAsync(async (req, res, next) => {
         cartId: cart.id,
         userId: sessionUser.id,
         totalPrice: totalPrice,
-        
     });
 
     res.status(200).json({
