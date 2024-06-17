412b3e92c67e613da9e19b98ca3032003bbf48d9
rony171998 <rony171998@gmail.com>
2024-06-16 19:48:06 -0500

    Update database.util.js

--- server/utils/database.util.js
+++ server/utils/database.util.js
@@ -3,13 +3,15 @@ const dotenv = require("dotenv").config();
 
 const db = new Sequelize({
     dialect: "postgres",
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    port: process.env.DB_PORT,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-    dbname: process.env.DB,
-    user: process.env.DB_USER,
+    host:
+        process.env.DB_HOST |
+        "database-smark-mark.ch2ss8uqqwt1.us-east-2.rds.amazonaws.com",
+    username: process.env.DB_USER | "postgres",
+    port: process.env.DB_PORT | "5432",
+    password: process.env.DB_PASSWORD | "17041998",
+    database: process.env.DB | "smart-mark",
+    dbname: process.env.DB | "smart-mark",
+    user: process.env.DB_USER | "postgres",
     logging: false,
     dialectOptions:
         process.env.NODE_ENV === "production"
